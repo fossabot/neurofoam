5fb4dc32e445c03da90196d0ea3a599021f4da85
jameswilddev <jameswilddev@outlook.com>
2019-12-26 21:31:25 +0000

    Reduce runMain "chattiness".

--- @neurofoam/command-line-helpers/run-main-tests.ts
+++ @neurofoam/command-line-helpers/run-main-tests.ts
@@ -8,7 +8,6 @@ describe(`@neurofoam/command-line-helpers`, () => {
       assert: (
         main: jasmine.Spy,
         console: object,
-        consoleLog: jasmine.Spy,
         consoleError: jasmine.Spy,
         process: object,
         processExit: jasmine.Spy,
@@ -17,10 +16,8 @@ describe(`@neurofoam/command-line-helpers`, () => {
       describe(description, () => {
         const main = jasmine.createSpy(`main`).and.callFake(mainBody)
 
-        const consoleLog = jasmine.createSpy(`console.log`)
         const consoleError = jasmine.createSpy(`console.error`)
         const mockConsole = {
-          log: consoleLog,
           error: consoleError,
         }
         const originalConsole = console
@@ -46,7 +43,6 @@ describe(`@neurofoam/command-line-helpers`, () => {
         assert(
           main,
           mockConsole,
-          consoleLog,
           consoleError,
           mockProcess,
           processExit
@@ -57,11 +53,10 @@ describe(`@neurofoam/command-line-helpers`, () => {
     withFakedGlobals(
       `when the promise does not resolve or reject`,
       () => new Promise<void>(() => { }),
-      (main, console, consoleLog, consoleError, process, processExit) => {
+      (main, console, consoleError, process, processExit) => {
         console
         process
         it(`executes main once`, () => expect(main).toHaveBeenCalledTimes(1))
-        it(`does not log success`, () => expect(consoleLog).not.toHaveBeenCalled())
         it(`does not log failure`, () => expect(consoleError).not.toHaveBeenCalled())
         it(`does not exit`, () => expect(processExit).not.toHaveBeenCalled())
       }
@@ -70,16 +65,13 @@ describe(`@neurofoam/command-line-helpers`, () => {
     withFakedGlobals(
       `when the promise resolves`,
       async () => { },
-      (main, console, consoleLog, consoleError, process, processExit) => {
+      (main, console, consoleError, process, processExit) => {
+        console
         it(`executes main once`, () => expect(main).toHaveBeenCalledTimes(1))
-        it(`logs success once`, () => expect(consoleLog).toHaveBeenCalledTimes(1))
-        it(`uses the correct "this" when logging success`, () => expect(consoleLog.calls.first().object).toBe(console))
-        it(`logs the expected message`, () => expect(consoleLog).toHaveBeenCalledWith(`Done.`))
         it(`does not log failure`, () => expect(consoleError).not.toHaveBeenCalled())
         it(`exits once`, () => expect(processExit).toHaveBeenCalledTimes(1))
         it(`uses the correct "this" when exiting`, () => expect(processExit.calls.first().object).toBe(process))
         it(`exits successfully`, () => expect(processExit).toHaveBeenCalledWith(0))
-        it(`exits after logging`, () => expect(consoleLog).toHaveBeenCalledBefore(processExit))
       }
     )
 
@@ -88,11 +80,9 @@ describe(`@neurofoam/command-line-helpers`, () => {
       async () => {
         throw `Test Reason`
       },
-      (main, console, consoleLog, consoleError, process, processExit) => {
+      (main, console, consoleError, process, processExit) => {
         it(`executes main once`, () => expect(main).toHaveBeenCalledTimes(1))
-        it(`does not log success`, () => expect(consoleLog).not.toHaveBeenCalled())
-        it(`logs failure once`, () => expect(consoleError).toHaveBeenCalledTimes(2))
-        it(`logs that a failure occurred`, () => expect(consoleError).toHaveBeenCalledWith(`Failed.`))
+        it(`logs failure once`, () => expect(consoleError).toHaveBeenCalledTimes(1))
         it(`uses the correct "this" when logging that a failure occurred`, () => expect(consoleError.calls.first().object).toBe(console))
         it(`logs the reason for the failure`, () => expect(consoleError).toHaveBeenCalledWith(`Test Reason`))
         it(`uses the correct "this" when logging the reason for the failure`, () => expect(consoleError.calls.mostRecent().object).toBe(console))
--- @neurofoam/command-line-helpers/run-main.ts
+++ @neurofoam/command-line-helpers/run-main.ts
@@ -4,11 +4,9 @@ export default function (
   main()
     .then(
       () => {
-        console.log(`Done.`)
         process.exit(0)
       },
       reason => {
-        console.error(`Failed.`)
         console.error(reason)
         process.exit(1)
       }
